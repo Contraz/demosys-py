c29aa519a73ae2dadb684b84c9f2177bf943b33f
Einar Forselv <eforselv@gmail.com>
2017-04-08 21:55:31 +0200

    Effect registry: Split up effect loading to allow hacking the loader

--- demosys/effects/registry.py
+++ demosys/effects/registry.py
@@ -6,6 +6,10 @@ from demosys.effects.effect import Effect
 EFFECT_MODULE = 'effect'
 
 
+class EffectError(Exception):
+    pass
+
+
 class EffectConfig:
     def __init__(self, module=None, cls=None):
         self.module = module
@@ -29,21 +33,34 @@ class Effects:
             yield v.path
 
     def polulate(self, effect_list):
-        """
-        Load all effects
-        """
+        """Find all effects"""
         for effect in effect_list:
-            module = importlib.import_module(f'{effect}.{EFFECT_MODULE}')
-            # Find the Effect class in the module
-            for name, cls in inspect.getmembers(module):
-                if inspect.isclass(cls):
-                    if cls == Effect:
-                        continue
-                    # Use MRO to figure out if this is really an effect
-                    mro = inspect.getmro(cls)
-                    if cls in mro and Effect in mro:
-                        cls.name = effect
-                        self.effects[module.__name__] = EffectConfig(module=module, cls=cls)
+            name = f'{effect}.{EFFECT_MODULE}'
+            module, cls = self.get_effect_cls(name)
+            if cls:
+                cls.name = effect
+                self.effects[module.__name__] = EffectConfig(module=module, cls=cls)
+            else:
+                raise EffectError(f"Effect '{effect}' has no effect class")
+
+    def get_effect_cls(self, module_name):
+        """Find and return an effect class in a module
+        :param module_name: Name of the module
+        :returns: module, cls tuple
+        """
+        module = importlib.import_module(module_name)
+
+        # Find the Effect class in the module
+        for name, cls in inspect.getmembers(module):
+            if inspect.isclass(cls):
+                if cls == Effect:
+                    continue
+                # Use MRO to figure out if this is really an effect
+                mro = inspect.getmro(cls)
+                if cls in mro and Effect in mro:
+                    return module, cls
+
+        return None, None
 
 
 effects = Effects()
