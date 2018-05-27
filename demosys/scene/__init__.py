61dd3d80d4196dc5785da28109a9139cdbb09f9e
Einar Forselv <eforselv@gmail.com>
2018-05-28 01:51:21 +0200

    Basic texture material

--- demosys/scene/__init__.py
+++ demosys/scene/__init__.py
@@ -2,4 +2,4 @@ from .scene import Scene  # noqa
 from .node import Node  # noqa
 from .camera import Camera  # noqa
 from .mesh import Mesh  # noqa
-from .material import Material  # noqa
+from .material import Material, MaterialTexture  # noqa
--- demosys/scene/material.py
+++ demosys/scene/material.py
@@ -3,5 +3,11 @@
 class Material:
     def __init__(self, name):
         self.name = name
-        self.texture = None
         self.color = None
+        self.mat_texture = None
+
+
+class MaterialTexture:
+    def __init__(self, texture=None, sampler=None):
+        self.texture = texture
+        self.sampler = sampler
