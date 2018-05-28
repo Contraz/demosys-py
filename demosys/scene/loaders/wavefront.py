6b1a39aeb02b7f5d68291fef339bc9d47de3e7e6
Einar Forselv <eforselv@gmail.com>
2018-05-29 01:05:49 +0200

    Basic obj loader supporting positions

--- demosys/scene/loaders/wavefront.py
+++ demosys/scene/loaders/wavefront.py
@@ -1,4 +1,11 @@
+import numpy
 from .base import SceneLoader
+import pywavefront
+
+from OpenGL import GL
+from OpenGL.arrays.vbo import VBO
+from demosys.opengl import VAO
+from demosys.scene import Mesh, Node
 
 
 class ObjLoader(SceneLoader):
@@ -6,9 +13,26 @@ class ObjLoader(SceneLoader):
     file_extensions = ['.obj']
 
     def __init__(self, file_path):
-        self.path = path
         super().__init__(file_path)
 
     def load(self, scene, file=None):
-        print(scene, file)
+        """Deferred loading"""
+        data = pywavefront.Wavefront(file)
+
+        for mesh in data.mesh_list:
+            for mat in mesh.materials:
+                vbo = VBO(numpy.array(mat.vertices, dtype=numpy.dtype(numpy.float32)))
+
+                vao = VAO("moo", mode=GL.GL_TRIANGLES)
+                vao.add_array_buffer(GL.GL_FLOAT, vbo)
+                vao.map_buffer(vbo, "in_uv", 2)
+                vao.map_buffer(vbo, "in_normal", 3)
+                vao.map_buffer(vbo, "in_position", 3)
+                vao.build()
+
+                mesh = Mesh("moo", vao=vao)
+                scene.meshes.append(mesh)
+                node = Node(mesh=mesh)
+                scene.root_nodes.append(node)
+
         return scene
