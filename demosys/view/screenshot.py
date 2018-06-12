9fbd4fb4e74e7e6888b4b6bd2f534273125e909d
Einar Forselv <eforselv@gmail.com>
2018-06-12 22:41:14 +0200

    screenshot improvements
    - Use window buffer size as screenshow size
    - Create screenshot directory if not present
    - Allow overriding name

--- demosys/view/screenshot.py
+++ demosys/view/screenshot.py
@@ -5,28 +5,34 @@ from OpenGL import GL
 from demosys.conf import settings
 
 
-def create(format='png'):
+def create(format='png', name=None):
     """
     Create a screenshot
     :param format: formats supported by PIL (png, jpeg etc)
     """
+    # Avoid circular import
+    from demosys.view.controller import WINDOW
+
     dest = ""
-    if not settings.SCREENSHOT_PATH:
+    if settings.SCREENSHOT_PATH:
+        if not os.path.exists(settings.SCREENSHOT_PATH):
+            print("SCREENSHOT_PATH does not exist. creating: {}".format(settings.SCREENSHOT_PATH))
+            os.makedirs(settings.SCREENSHOT_PATH)
+        dest = settings.SCREENSHOT_PATH
+    else:
         print("SCREENSHOT_PATH not defined in settings. Using cwd as fallback.")
 
-    if settings.SCREENSHOT_PATH:
-        if os.path.exists(settings.SCREENSHOT_PATH):
-            dest = settings.SCREENSHOT_PATH
-        else:
-            print("SCREENSHOT_PATH {} does not exist. Using cwd as fallback".format(settings.SCREENSHOT_PATH))
+    # x, y, width, height = GL.glGetIntegerv(GL.GL_VIEWPORT)
+    x, y, width, height = 0, 0, WINDOW.buffer_width, WINDOW.buffer_height
 
-    x, y, width, height = GL.glGetIntegerv(GL.GL_VIEWPORT)
-    print("Screenshot viewport:", x, y, width, height)
     GL.glPixelStorei(GL.GL_PACK_ALIGNMENT, 1)
 
     data = GL.glReadPixels(x, y, width, height, GL.GL_RGB, GL.GL_UNSIGNED_BYTE)
 
     image = Image.frombytes("RGB", (width, height), data)
     image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    name = "{}.{}".format(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"), format)
+    if not name:
+        name = "{}.{}".format(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"), format)
+
+    print("Creating screenshot:", name)
     image.save(os.path.join(dest, name), format=format)
