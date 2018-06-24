1da2a91fb2d6748a22ff810fe8fa282a7a593dac
Einar Forselv <eforselv@gmail.com>
2018-06-24 02:01:13 +0200

    Update VAO creating in wavefront loader

--- demosys/scene/loaders/wavefront.py
+++ demosys/scene/loaders/wavefront.py
@@ -3,7 +3,6 @@ from .base import SceneLoader
 import pywavefront
 
 from OpenGL import GL
-from OpenGL.arrays.vbo import VBO
 from demosys.opengl import VAO
 from demosys.scene import Mesh, Node, Material, MaterialTexture
 from demosys.resources import textures
@@ -19,27 +18,43 @@ class ObjLoader(SceneLoader):
 
     def load(self, scene, file=None):
         """Deferred loading"""
-        data = pywavefront.Wavefront(file)
+        data = pywavefront.Wavefront(file, create_materials=True)
 
         for name, mat in data.materials.items():
-            vbo = VBO(numpy.array(mat.vertices, dtype=numpy.dtype(numpy.float32)))
+
+            if not mat.vertices:
+                continue
+
+            vbo = numpy.array(mat.vertices, dtype=numpy.float32)
 
             vao = VAO(mat.name, mode=GL.GL_TRIANGLES)
-            vao.add_array_buffer(GL.GL_FLOAT, vbo)
-            mesh = Mesh(mat.name, vao=vao)
+            mesh = Mesh(mat.name)
+
+            # Order: T2F, C3F, N3F and V3F
+            buffer_format = []
+            attributes = []
 
             if "T2F" in mat.vertex_format:
-                vao.map_buffer(vbo, "in_uv", 2)
+                buffer_format.append("2f")
+                attributes.append("in_uv")
                 mesh.add_attribute("TEXCOORD_0", "in_uv", 2)
 
+            if "C3F" in mat.vertex_format:
+                buffer_format.append("3f")
+                attributes.append("in_color")
+                mesh.add_attribute("NORMAL", "in_color", 3)
+
             if "N3F" in mat.vertex_format:
-                vao.map_buffer(vbo, "in_normal", 3)
+                buffer_format.append("3f")
+                attributes.append("in_normal")
                 mesh.add_attribute("NORMAL", "in_normal", 3)
 
-            vao.map_buffer(vbo, "in_position", 3)
+            buffer_format.append("3f")
+            attributes.append("in_position")
             mesh.add_attribute("POSITION", "in_position", 3)
 
-            vao.build()
+            vao.buffer(vbo, " ".join(buffer_format), attributes)
+            mesh.vao = vao
 
             scene.meshes.append(mesh)
 
