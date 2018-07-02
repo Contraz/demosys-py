0b17f6e70a1b69e78ee88c2a1eed77eb21061492
Einar Forselv <eforselv@gmail.com>
2018-07-03 01:26:37 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="demosys-py",
-    version="1.0.1",
+    version="1.0.2",
     description="Modern OpenGL 3.3+ Framework inspired by Django",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
