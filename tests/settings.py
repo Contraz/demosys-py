0f4fb353cfb4986337509974e1cd00770847e772
Einar Forselv <eforselv@gmail.com>
2018-07-05 17:42:53 +0200

    test settings

---
+++ tests/settings.py
@@ -0,0 +1,60 @@
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DEBUG = False
+
+SCREENSHOT_PATH = os.path.join(PROJECT_DIR, 'screenshots')
+
+# Profile: any, core, compat
+OPENGL = {
+    "version": (3, 3),
+    "profile": "core",
+    "forward_compat": True,
+}
+
+WINDOW = {
+    "size": (1280, 720),
+    "vsync": True,
+    "resizable": True,
+    "fullscreen": False,
+    "title": "demosys-py",
+    "cursor": True,
+}
+
+# MUSIC = os.path.join(PROJECT_DIR, 'resources/music/tg2035.mp3')
+TIMER = 'demosys.timers.Timer'
+# TIMER = 'demosys.timers.RocketTimer'
+# TIMER = 'demosys.timers.RocketMusicTimer'
+# TIMER = 'demosys.timers.MusicTimer'
+
+ROCKET = {
+    'mode': 'project',
+    'rps': 60,
+    'project': os.path.join(PROJECT_DIR, 'resources', 'cube.xml'),
+    'files': os.path.join(PROJECT_DIR, 'resources', 'tracks'),
+}
+
+# What effects to load
+EFFECTS = (
+)
+
+SHADER_DIRS = (
+    os.path.join(PROJECT_DIR, 'resources/shaders'),
+)
+
+SHADER_FINDERS = (
+    'demosys.core.shaderfiles.finders.FileSystemFinder',
+    'demosys.core.shaderfiles.finders.EffectDirectoriesFinder',
+)
+
+# Hardcoded paths to shader dirs
+TEXTURE_DIRS = (
+    os.path.join(PROJECT_DIR, 'resource/textures'),
+)
+
+# Finder classes
+TEXTURE_FINDERS = (
+    'demosys.core.texturefiles.finders.FileSystemFinder',
+    'demosys.core.texturefiles.finders.EffectDirectoriesFinder'
+)
--- tests/test_docs.py
+++ tests/test_docs.py
@@ -10,33 +10,36 @@ import os
 import re
 import unittest
 
-import moderngl
+os.environ['DEMOSYS_SETTINGS_MODULE'] = 'tests.settings'
+
+import demosys
+from demosys import effects
 
 
 class TestCase(unittest.TestCase):
 
-    def validate(self, filename, classname, ignore):
+    def validate(self, filename, module, classname, ignore):
         root = os.path.dirname(os.path.dirname(__file__))
-        with open(os.path.normpath(os.path.join(root, 'docs', 'reference', filename))) as f:
+        with open(os.path.normpath(os.path.join(root, 'docs', 'source', filename))) as f:
             docs = f.read()
         methods = re.findall(r'^\.\. automethod:: ([^\(\n]+)([^\n]+)', docs, flags=re.M)
         attributes = re.findall(r'^\.\. autoattribute:: ([^\n]+)', docs, flags=re.M)
         documented = set(filter(lambda x: x.startswith(classname), [a for a, b in methods] + attributes))
-        implemented = set(classname + '.' + x for x in dir(getattr(moderngl, classname)) if not x.startswith('_'))
+        implemented = set(classname + '.' + x for x in dir(getattr(module, classname)) if not x.startswith('_'))
         ignored = set(classname + '.' + x for x in ignore)
         self.assertSetEqual(implemented - documented - ignored, set(), msg='Implemented but not Documented')
         self.assertSetEqual(documented - implemented, set(), msg='Documented but not Implemented')
 
         for method, docsig in methods:
             classname, methodname = method.split('.')
-            sig = str(inspect.signature(getattr(getattr(moderngl, classname), methodname)))
+            sig = str(inspect.signature(getattr(getattr(module, classname), methodname)))
             sig = sig.replace('self, ', '').replace('moderngl.', '').replace('typing.', '').replace(' -> None', '')
             sig = sig.replace('(self)', '()').replace(', *,', ',').replace('(*, ', '(')
             sig = re.sub(r'-> \'(\w+)\'', r'-> \1', sig)
             self.assertEqual(docsig, sig, msg=filename + '::' + method)
 
     def test_context_docs(self):
-        self.validate('effect.rst', 'Effect', ['test', 'test2'])
+        self.validate('reference/effect.rst', effects, 'Effect', [])
 
 
 if __name__ == '__main__':
