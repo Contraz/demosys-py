5e8d2d89237a6eba2e3d136a9e0ea62875ef481e
Einar Forselv <eforselv@gmail.com>
2018-07-12 01:17:09 +0200

    demosys.timer.Timer should not be based on glfw time

--- demosys/timers/time.py
+++ demosys/timers/time.py
@@ -1,11 +1,11 @@
-import glfw
+import time
 
 from .base import BaseTimer
 
 
 class Timer(BaseTimer):
     """
-    Timer based on glfw time.
+    Timer based on python ``time``.
     This is the default / most basic timer.
     """
     def __init__(self, **kwargs):
@@ -18,11 +18,11 @@ class Timer(BaseTimer):
     def start(self):
         """Start the timer"""
         if self.start_time is None:
-            self.start_time = glfw.get_time()
+            self.start_time = time.time()
         # Play after pause
         else:
             # Add the duration of the paused interval to the total offset
-            pause_duration = glfw.get_time() - self.pause_time
+            pause_duration = time.time() - self.pause_time
             self.offset += pause_duration
             # print("pause duration", pause_duration, "offset", self.offset)
             # Exit the paused state
@@ -30,7 +30,7 @@ class Timer(BaseTimer):
 
     def pause(self):
         """Pause the timer"""
-        self.pause_time = glfw.get_time()
+        self.pause_time = time.time()
 
     def toggle_pause(self):
         """Toggle pause"""
@@ -41,14 +41,14 @@ class Timer(BaseTimer):
 
     def stop(self):
         """Stop the timer"""
-        self.stop_time = glfw.get_time()
+        self.stop_time = time.time()
         return self.stop_time - self.start_time - self.offset
 
     def get_time(self):
         """Get the current time in seconds"""
         if self.pause_time is not None:
-            time = self.pause_time - self.offset - self.start_time
-            return time
+            curr_time = self.pause_time - self.offset - self.start_time
+            return curr_time
 
-        time = glfw.get_time()
-        return time - self.start_time - self.offset
+        curr_time = time.time()
+        return curr_time - self.start_time - self.offset
