dc0a77e703ed203ed0ba8f60c930e6d15fa49dc3
Frode Ulsund <fr@68k.no>
2018-07-16 05:11:57 +0200

    Allow setting cube center point

--- demosys/geometry/cube.py
+++ demosys/geometry/cube.py
@@ -3,56 +3,57 @@ import numpy
 from demosys.opengl import VAO
 
 
-def cube(width, height, depth, normals=True, uvs=True):
+def cube(width, height, depth, center=(0.0, 0.0, 0.0), normals=True, uvs=True) -> VAO:
     """
-    Generates a cube VAO
+    Generates a cube VAO. The cube center is (0.0, 0.0 0.0) unless other is specified.
 
     :param width: Width of the cube
     :param height: height of the cube
     :param depth: depth of the cube
+    :param center: center of the cube
     :param normals: (bool) Include normals
     :param uvs: (bool) include uv coordinates
     :return: VAO representing the cube
     """
     width, height, depth = width / 2.0, height / 2.0, depth / 2.0
-
+    
     pos = numpy.array([
-        width, -height, depth,
-        width, height, depth,
-        -width, -height, depth,
-        width, height, depth,
-        -width, height, depth,
-        -width, -height, depth,
-        width, -height, -depth,
-        width, height, -depth,
-        width, -height, depth,
-        width, height, -depth,
-        width, height, depth,
-        width, -height, depth,
-        width, -height, -depth,
-        width, -height, depth,
-        -width, -height, depth,
-        width, -height, -depth,
-        -width, -height, depth,
-        -width, -height, -depth,
-        -width, -height, depth,
-        -width, height, depth,
-        -width, height, -depth,
-        -width, -height, depth,
-        -width, height, -depth,
-        -width, -height, -depth,
-        width, height, -depth,
-        width, -height, -depth,
-        -width, -height, -depth,
-        width, height, -depth,
-        -width, -height, -depth,
-        -width, height, -depth,
-        width, height, -depth,
-        -width, height, -depth,
-        width, height, depth,
-        -width, height, -depth,
-        -width, height, depth,
-        width, height, depth,
+        center[0] + width, center[1] - height, center[2] + depth,
+        center[0] + width, center[1] + height, center[2] + depth,
+        center[0] - width, center[1] - height, center[2] + depth,
+        center[0] + width, center[1] + height, center[2] + depth,
+        center[0] - width, center[1] + height, center[2] + depth,
+        center[0] - width, center[1] - height, center[2] + depth,
+        center[0] + width, center[1] - height, center[2] - depth,
+        center[0] + width, center[1] + height, center[2] - depth,
+        center[0] + width, center[1] - height, center[2] + depth,
+        center[0] + width, center[1] + height, center[2] - depth,
+        center[0] + width, center[1] + height, center[2] + depth,
+        center[0] + width, center[1] - height, center[2] + depth,
+        center[0] + width, center[1] - height, center[2] - depth,
+        center[0] + width, center[1] - height, center[2] + depth,
+        center[0] - width, center[1] - height, center[2] + depth,
+        center[0] + width, center[1] - height, center[2] - depth,
+        center[0] - width, center[1] - height, center[2] + depth,
+        center[0] - width, center[1] - height, center[2] - depth,
+        center[0] - width, center[1] - height, center[2] + depth,
+        center[0] - width, center[1] + height, center[2] + depth,
+        center[0] - width, center[1] + height, center[2] - depth,
+        center[0] - width, center[1] - height, center[2] + depth,
+        center[0] - width, center[1] + height, center[2] - depth,
+        center[0] - width, center[1] - height, center[2] - depth,
+        center[0] + width, center[1] + height, center[2] - depth,
+        center[0] + width, center[1] - height, center[2] - depth,
+        center[0] - width, center[1] - height, center[2] - depth,
+        center[0] + width, center[1] + height, center[2] - depth,
+        center[0] - width, center[1] - height, center[2] - depth,
+        center[0] - width, center[1] + height, center[2] - depth,
+        center[0] + width, center[1] + height, center[2] - depth,
+        center[0] - width, center[1] + height, center[2] - depth,
+        center[0] + width, center[1] + height, center[2] + depth,
+        center[0] - width, center[1] + height, center[2] - depth,
+        center[0] - width, center[1] + height, center[2] + depth,
+        center[0] + width, center[1] + height, center[2] + depth,
     ], dtype=numpy.float32)
 
     if normals:
