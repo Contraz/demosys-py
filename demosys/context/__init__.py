99f2fb88315abb48d901025f7b8d1787adedc47a
Einar Forselv <eforselv@gmail.com>
2018-07-21 17:34:37 +0200

    Better feedback when getting window or context before creation

--- demosys/context/__init__.py
+++ demosys/context/__init__.py
@@ -6,9 +6,16 @@ WINDOW = None  # noqa
 
 def window() -> 'demosys.context.base.Window':
     """The window instance we are rendering to"""
+    if not WINDOW:
+        raise RuntimeError("Attempting to get window before creation")
+
     return WINDOW
 
 
 def ctx() -> moderngl.Context:
     """ModernGL context"""
-    return WINDOW.ctx
+    win = window()
+    if not win.ctx:
+        raise RuntimeError("Attempting to get context before creation")
+
+    return win.ctx
