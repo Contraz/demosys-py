6121fed0d6f23b07ae99eb8af3c685b75bd4e2bc
Einar Forselv <eforselv@gmail.com>
2018-07-21 23:32:28 +0200

    Revamp finders

--- demosys/core/scenefiles/finders.py
+++ demosys/core/scenefiles/finders.py
@@ -5,34 +5,15 @@ from demosys.conf import settings
 from demosys.core.exceptions import ImproperlyConfigured
 from demosys.utils.module_loading import import_string
 
-SCENE_DIR = 'scenes'
 
-
-class FileSystemFinder(finders.FileSystemFinder):
+class FileSystemFinder(finders.BaseFileSystemFinder):
     """Find textures in ``SCENE_DIRS``"""
-    def __init__(self):
-        if not hasattr(settings, 'SCENE_DIRS'):
-            raise ImproperlyConfigured(
-                "Settings module don't define SCENE_DIRS."
-                "This is required when using a FileSystemFinder."
-            )
-        super().__init__(settings.TEXTURE_DIRS)
-
-    # TODO: Use values from settings to filter files
-    # def find(self, path):
-    #     pass
+    settings_attr = 'SCENE_DIRS'
 
 
-class EffectDirectoriesFinder(finders.FileSystemFinder):
+class EffectDirectoriesFinder(finders.BaseEffectDirectoriesFinder):
     """Finds textures in the registered effects"""
-    def __init__(self):
-        from demosys.effects.registry import effects
-        dirs = list(effects.get_dirs())
-        super().__init__(dirs)
-
-    # TODO: Use values from settings to filter texture files
-    def find(self, path):
-        return self._find(os.path.join(SCENE_DIR, path))
+    directory = 'scenes'
 
 
 def get_finders():
@@ -51,6 +32,6 @@ def get_finder(import_path):
     :return: An instance of the finder
     """
     Finder = import_string(import_path)
-    if not issubclass(Finder, finders.FileSystemFinder):
+    if not issubclass(Finder, finders.BaseFileSystemFinder):
         raise ImproperlyConfigured('Finder {} is not a subclass of core.finders.FileSystemFinder'.format(import_path))
     return Finder()
--- demosys/core/texturefiles/finders.py
+++ demosys/core/texturefiles/finders.py
@@ -5,34 +5,15 @@ from demosys.conf import settings
 from demosys.core.exceptions import ImproperlyConfigured
 from demosys.utils.module_loading import import_string
 
-TEXTURE_DIR = 'textures'
 
-
-class FileSystemFinder(finders.FileSystemFinder):
+class FileSystemFinder(finders.BaseFileSystemFinder):
     """Find textures in ``TEXTURE_DIRS``"""
-    def __init__(self):
-        if not hasattr(settings, 'TEXTURE_DIRS'):
-            raise ImproperlyConfigured(
-                "Settings module don't define TEXTURE_DIRS."
-                "This is required when using a FileSystemFinder."
-            )
-        super().__init__(settings.TEXTURE_DIRS)
-
-    # TODO: Use values from settings to filter texture files
-    # def find(self, path):
-    #     pass
+    settings_attr = 'TEXTURE_DIRS'
 
 
-class EffectDirectoriesFinder(finders.FileSystemFinder):
+class EffectDirectoriesFinder(finders.BaseEffectDirectoriesFinder):
     """Finds textures in the registered effects"""
-    def __init__(self):
-        from demosys.effects.registry import effects
-        dirs = list(effects.get_dirs())
-        super().__init__(dirs)
-
-    # TODO: Use values from settings to filter texture files
-    def find(self, path):
-        return self._find(os.path.join(TEXTURE_DIR, path))
+    directory = 'textures'
 
 
 def get_finders():
@@ -51,6 +32,6 @@ def get_finder(import_path):
     :return: An instance of the finder
     """
     Finder = import_string(import_path)
-    if not issubclass(Finder, finders.FileSystemFinder):
+    if not issubclass(Finder, finders.BaseFileSystemFinder):
         raise ImproperlyConfigured('Finder {} is not a subclass of core.finders.FileSystemFinder'.format(import_path))
     return Finder()
