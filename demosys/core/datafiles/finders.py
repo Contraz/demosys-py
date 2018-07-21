24e625fa19dedc89771fffcdc3cdff5b0fdeb725
Einar Forselv <eforselv@gmail.com>
2018-07-21 23:37:19 +0200

    Data finder

---
+++ demosys/core/datafiles/finders.py
@@ -0,0 +1,37 @@
+import functools
+
+from demosys.core import finders
+from demosys.conf import settings
+from demosys.core.exceptions import ImproperlyConfigured
+from demosys.utils.module_loading import import_string
+
+
+class FileSystemFinder(finders.BaseFileSystemFinder):
+    """Find data in ``DATA_DIRS``"""
+    settings_attr = 'DATA_DIRS'
+
+
+class EffectDirectoriesFinder(finders.BaseEffectDirectoriesFinder):
+    """Finds data in the registered effects"""
+    directory = 'data'
+
+
+def get_finders():
+    for finder in settings.TEXTURE_FINDERS:
+        yield get_finder(finder)
+
+
+@functools.lru_cache(maxsize=None)
+def get_finder(import_path):
+    """
+    Get a finder class from an import path.
+    Raises ``demosys.core.exceptions.ImproperlyConfigured`` if the finder is not found.
+    This function uses an lru cache.
+
+    :param import_path: string representing an import path
+    :return: An instance of the finder
+    """
+    Finder = import_string(import_path)
+    if not issubclass(Finder, finders.FileSystemFinder):
+        raise ImproperlyConfigured('Finder {} is not a subclass of core.finders.FileSystemFinder'.format(import_path))
+    return Finder()
