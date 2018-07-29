7c2f1e8c6e9482642ad3a94ba6ed1b6afd320ddb
Einar Forselv <eforselv@gmail.com>
2018-07-29 05:50:40 +0200

    Load textures in their original format
    We should not auto convert all images to RGBA. Images should be loaded as is. This also speeds up loading

--- demosys/opengl/texture/base.py
+++ demosys/opengl/texture/base.py
@@ -4,6 +4,16 @@ import moderngl
 from demosys import context
 
 
+def image_data(image):
+    """Get components and bytes for an image"""
+    # NOTE: We might want to check the actual image.mode
+    #       and convert to an acceptable format.
+    #       At the moment we load the data as is.
+    data = image.tobytes()
+    components = len(data) // (image.size[0] * image.size[1])
+    return components, data
+
+
 class BaseTexture:
     """
     Wraps the basic functionality of the ModernGL methods
