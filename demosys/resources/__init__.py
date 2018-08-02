9d2c29666996486d55dbfef06f43e4dfad51f5fb
Einar Forselv <eforselv@gmail.com>
2018-08-02 20:19:46 +0200

    Remove all resource callbacks

--- demosys/resources/__init__.py
+++ demosys/resources/__init__.py
@@ -20,49 +20,12 @@ __all__ = [
 ]
 
 
-class States:
-    on_loaded_funcs = []
-    on_load_funcs = []
-    loaded = False
-
-    @classmethod
-    def sort_callbacks(cls):
-        cls.on_load_funcs = sorted(cls.on_load_funcs, key=lambda x: x[0])
-        cls.on_loaded_funcs = sorted(cls.on_loaded_funcs, key=lambda x: x[0])
-
-
 def load():
-    States.sort_callbacks()
-
-    for func in reversed(States.on_load_funcs):
-        func[1]()
-
-    scenes.load()
-    shaders.load()
-    textures.load()
-    tracks.load()
-    data.load()
-
-    States.loaded = True
-    for func in reversed(States.on_loaded_funcs):
-        func[1]()
+    scenes.load_pool()
+    shaders.load_pool()
+    textures.load_pool()
+    data.load_pool()
 
 
 def count():
-    return shaders.count + textures.count
-
-
-def loading_complete():
-    return States.loaded
-
-
-def on_load(func, priority=0):
-    States.on_load_funcs.append((priority, func))
-
-
-def on_loaded(func, priority=0):
-    if loading_complete():
-        func()
-        return
-
-    States.on_loaded_funcs.append((priority, func))
+    return scenes.count + shaders.count + textures.count + data.count
