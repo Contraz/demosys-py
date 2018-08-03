81bcefff1f8a2a600c21e7b5407f9eb8ccf28cc8
einarf <eforselv@gmail.com>
2018-08-03 22:45:10 +0200

    Inject deferred shaders instead of faking an effect

--- demosys/deferred/__init__.py
+++ demosys/deferred/__init__.py
@@ -1,1 +1,9 @@
+import os
+
+from demosys.conf import settings
+
+settings.add_shader_dir(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'shaders')
+)
+
 from .renderer import DeferredRenderer  # noqa
--- demosys/deferred/effect.py
+++
@@ -1,8 +0,0 @@
-# HACK: This module is here so the shaders can be found when adding the package as an effect
-from demosys.effects import Effect
-
-
-class DeferredEffect(Effect):
-
-    def draw(self, time, frametime, target):
-        pass
