4b4d65b52bc55501bc6b4ed83d5ac09db52e079f
einarf <eforselv@gmail.com>
2018-08-09 03:04:36 +0200

    Make scene materials more pleasant in debugger

--- demosys/scene/material.py
+++ demosys/scene/material.py
@@ -7,6 +7,12 @@ class Material:
         self.mat_texture = None
         self.double_sided = True
 
+    def __str__(self):
+        return "<Material {}>".format(self.name)
+
+    def __repr__(self):
+        return str(self)
+
 
 class MaterialTexture:
     def __init__(self, texture=None, sampler=None):
