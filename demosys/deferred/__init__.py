8e7827d982041f764013ea95c02625dbd28b0cf1
einarf <eforselv@gmail.com>
2018-08-09 20:48:42 +0200

    Move deferred into effects

--- demosys/deferred/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from demosys.conf import settings
-
-settings.add_program_dir(
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'programs')
-)
-
-from .renderer import DeferredRenderer  # noqa
