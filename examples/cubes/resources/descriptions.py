ecd7f3083afe420a4a5485195c4249d65bbd9c3c
einarf <eforselv@gmail.com>
2018-08-09 20:51:58 +0200

    Convert cubes into new effect format

---
+++ examples/cubes/resources/descriptions.py
@@ -0,0 +1,9 @@
+from demosys.resources.meta import TextureDescription, ProgramDescription
+
+
+resources = [
+    ProgramDescription(path="cubes/cube_plain.glsl", label="plain"),
+    ProgramDescription(path="cubes/cube_light.glsl", label="light"),
+    ProgramDescription(path="cubes/cube_textured.glsl", label="textured"),
+    TextureDescription(path="cubes/crate.jpg", label="crate"),
+]
