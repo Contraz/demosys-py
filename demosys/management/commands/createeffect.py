c3cb61183b570886bc8b83eff2c1e34921473edc
einarf <eforselv@gmail.com>
2018-08-10 16:22:21 +0200

    root_path incorrect in createeffect

--- demosys/management/commands/createeffect.py
+++ demosys/management/commands/createeffect.py
@@ -74,7 +74,7 @@ def list_templates():
 def root_path():
     """Get the absolute path to the root of the demosys package"""
     module_dir = os.path.dirname(globals()['__file__'])
-    return os.path.dirname(os.path.dirname(os.path.dirname(module_dir)))
+    return os.path.dirname(os.path.dirname(module_dir))
 
 
 def template_dir():
