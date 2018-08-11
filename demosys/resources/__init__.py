03e07de189468f28d815406fbbe14e9721f5ddac
einarf <eforselv@gmail.com>
2018-08-12 01:32:55 +0200

    Remove old resources functions

--- demosys/resources/__init__.py
+++ demosys/resources/__init__.py
@@ -3,14 +3,3 @@ from demosys.resources.textures import textures  # noqa
 from demosys.resources.tracks import tracks  # noqa
 from demosys.resources.scenes import scenes  # noqa
 from demosys.resources.data import data  # noqa
-
-
-def load():
-    scenes.load_pool()
-    programs.load_pool()
-    textures.load_pool()
-    data.load_pool()
-
-
-def count():
-    return scenes.count + programs.count + textures.count + data.count
