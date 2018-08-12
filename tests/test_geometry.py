49c909810f35ac426b0aaebce2dd4ae17c581fc3
einarf <eforselv@gmail.com>
2018-08-12 05:14:16 +0200

    Working test_geometry

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -3,30 +3,32 @@ from demosys.test import DemosysTestCase
 
 
 class GeometryTest(DemosysTestCase):
+    program = None
 
     def setUp(self):
-        self.shader = self.create_program(path="vf_pos.glsl")
+        if not getattr(self, 'program'):
+            self.program = self.load_program(path="vf_pos.glsl")
 
     def test_bbox(self):
         vao = geometry.bbox()
-        vao.draw(self.shader)
+        vao.draw(self.program)
 
     def test_cube(self):
         vao = geometry.cube(1.0, 1.0, 1.0)
-        vao.draw(self.shader)
+        vao.draw(self.program)
 
     def test_plane(self):
         vao = geometry.plane_xz()
-        vao.draw(self.shader)
+        vao.draw(self.program)
 
     def test_points(self):
         vao = geometry.points_random_3d(1000)
-        vao.draw(self.shader)
+        vao.draw(self.program)
 
     def test_quad(self):
         vao = geometry.quad_fs()
-        vao.draw(self.shader)
+        vao.draw(self.program)
 
     def test_sphere(self):
         vao = geometry.sphere()
-        vao.draw(self.shader)
+        vao.draw(self.program)
