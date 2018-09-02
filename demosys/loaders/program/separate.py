694d02ac1e5624591dd042b4b4070a223a7af541
Einar Forselv <eforselv@gmail.com>
2018-09-02 20:13:22 +0200

    Handle geometry shaders in separate loader

--- demosys/loaders/program/separate.py
+++ demosys/loaders/program/separate.py
@@ -6,16 +6,19 @@ class Loader(BaseLoader):
     name = 'separate'
 
     def load(self):
-
         vs_source = self.load_shader("vertex", self.meta.vertex_shader)
         geo_source = self.load_shader("geometry", self.meta.geometry_shader)
         fs_source = self.load_shader("fragment", self.meta.fragment_shader)
+        tc_source = self.load_shader("tess_control", self.meta.tess_control_shader)
+        te_source = self.load_shader("tess_evaluation", self.meta.tess_evaluation_shader)
 
         shaders = program.ProgramShaders.from_separate(
             self.meta,
             vs_source,
             geometry_source=geo_source,
             fragment_source=fs_source,
+            tess_control_shader=tc_source,
+            tess_evaluation_shader=te_source,
         )
         prog = shaders.create()
 
@@ -34,6 +37,8 @@ class Loader(BaseLoader):
             resolved_path = self.find_program(path)
             if not resolved_path:
                 raise ValueError("Cannot find {} shader '{}'".format(shader_type, path))
+
+            print("Loading:", path)
 
             with open(resolved_path, 'r') as fd:
                 return fd.read()
