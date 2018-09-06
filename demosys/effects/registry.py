0fd7f6cb904cd61dcd8118f169109fa9361f3a8f
einarf <eforselv@gmail.com>
2018-09-06 19:12:54 +0200

    Generic parse_package_string function

--- demosys/effects/registry.py
+++ demosys/effects/registry.py
@@ -6,6 +6,34 @@ from typing import Any, List, Type
 from demosys.effects.effect import Effect
 
 
+def parse_package_string(path):
+    """
+    Parse the effect package string.
+    Can contain the package python path or path to effect class in an effect package.
+
+    Examples::
+
+        # Path to effect pacakge
+        examples.cubes
+
+        # Path to effect class
+        examples.cubes.Cubes
+
+    Args:
+        path: python path to effect package. May also include effect class name.
+
+    Returns:
+        tuple: (package_path, effect_class)
+    """
+    parts = path.split('.')
+
+    # Is the last entry in the path capitalized?
+    if parts[-1][0].isupper():
+        return ".".join(parts[:-1]), parts[-1]
+
+    return path
+
+
 class EffectRegistry:
     """
     Registry for effects classes (not instances).
