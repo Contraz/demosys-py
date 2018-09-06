c04b8932ec65480ba90dd4578d5f6bb8c3baa690
einarf <eforselv@gmail.com>
2018-09-06 20:00:43 +0200

    Improve errors when effect packages or effects are not found

--- demosys/project/default.py
+++ demosys/project/default.py
@@ -21,9 +21,17 @@ class Project(BaseProject):
         pass
 
     def create_effect_instances(self):
-        cls = effects.find_effect_class(self.path)
+        if self.effect_class_name:
+            cls = effects.find_effect_class(self.path)
+            if not cls.runnable:
+                raise ValueError("Effect doesn't have the runnable flag set:", self.path)
+        else:
+            effect_package = effects.get_package(self.effect_package_name)
+            runnable_effects = effect_package.runnable_effects()
 
-        if not cls.runnable:
-            raise ValueError("Effect doesn't have the runnable flag set:", self.path)
+            if not runnable_effects:
+                raise ValueError("No runnable effects found in effect package", self.effect_package_name)
+
+            cls = runnable_effects[-1]
 
         self.effect = self.create_effect('default', cls.__name__)
