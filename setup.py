e9992324f2e0c2f842ff0424309878cfec41892f
Einar Forselv <eforselv@gmail.com>
2019-01-02 23:40:35 +0100

    Indicate python 3.7 support

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
     ],
     install_requires=[
