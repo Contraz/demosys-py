86a07a3ff9de285b49c7f605b6b88ab84145ca67
Einar Forselv <eforselv@gmail.com>
2019-01-10 17:01:09 +0100

    doc typo

--- demosys/scene/mesh.py
+++ demosys/scene/mesh.py
@@ -56,7 +56,7 @@ class Mesh:
     def add_attribute(self, attr_type, name, components):
         """
         Add metadata about the mesh
-        :param attr_type: POSITION, NORMAL ec
+        :param attr_type: POSITION, NORMAL etc
         :param name: The attribute name used in the program
         :param components: Number of floats
         """
